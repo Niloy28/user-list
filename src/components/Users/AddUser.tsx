fd5b6ad83f51e3e4aafd85af98ceb375edb60ec4
Farhan Ishrak Islam <niloy28@live.com>
2023-02-23 20:31:27 +0600

    Added AddUser form.

---
+++ src/components/Users/AddUser.tsx
@@ -0,0 +1,19 @@
+const AddUser = () => {
+	const formSubmitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+	};
+
+	return (
+		<form onSubmit={formSubmitHandler}>
+			<label htmlFor="username">Username</label>
+			<input id="username" type="text" />
+
+			<label htmlFor="age">Age (Years)</label>
+			<input id="age" type="number" />
+
+			<button type="submit">Add User</button>
+		</form>
+	);
+};
+
+export default AddUser;
